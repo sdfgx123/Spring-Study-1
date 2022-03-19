6f28175da343346254337e469b622f83ec6b583c
sdfgx123 <sdfgx123@naver.com>
2022-03-20 02:31:02 +0900

    20220320 init

---
+++ core/src/main/java/hello/core/member/MemberApp.java
@@ -0,0 +1,16 @@
+package hello.core.member;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        // memberService 의 join 매서드를 통해 회원가입이 된다
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+
+    }
+}
---
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join() {
+        //given 이런이런 환경이 주어졌을 때
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        //when 이렇게 됐을 때
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then 저렇게 된다
+    }
+}
