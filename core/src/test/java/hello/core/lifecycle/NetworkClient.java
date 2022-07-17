e323f05139f8fe78aa7cf2dc8a8033770cb99fce
sdfgx123 <sdfgx123@naver.com>
2022-07-17 21:12:05 +0900

    20220717

---
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,33 @@
+package hello.core.lifecycle;
+
+public class NetworkClient {
+
+    //접속할 서버의 url
+    private String url;
+
+    //default 생성자
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    //외부에서 setter로 넣을 수 있게 만듦
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    //서비스 시작시 호출
+    public void connect() {
+        System.out.println("connect : " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call : " + url + "message : " + message);
+    }
+
+    //서비스 종료시 호출
+    public void disconnect() {
+        System.out.println("close : " + url);
+    }
+}
