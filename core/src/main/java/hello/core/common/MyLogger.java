8406d4dcf0d417bd8cbb17766bdde8955e9a4d79
sdfgx123 <sdfgx123@naver.com>
2022-07-31 19:22:44 +0900

    20220731

---
+++ core/src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,35 @@
+package hello.core.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+
+// 로그를 출력하기 위한 MyLogger 클래스, web scope를 통한 의존성 주입
+@Component
+@Scope(value = "request")
+public class MyLogger {
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "]" + "[" + message + "]");
+    }
+
+    @PostConstruct
+    public void init() {
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean created" + this);
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("[" + uuid + "] request scope bean closed" + this);
+    }
+}
---
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,32 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+// 생성자에 Autowired 자동으로 들어가면서 의존성 주입
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    // ResponseBody : 뷰 화면 없이 문자 그대로 반환시 사용
+    @RequestMapping("log-demo")
+    @ResponseBody
+    // 자바에서 제공하는 표준 서블릿 규약 > 이를 통해 고객의 request 정보를 받을 수 있음
+    public String logDemo(HttpServletRequest request) {
+        // 고객이 어떤 URL로 요청했는지 알 수 있음
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
