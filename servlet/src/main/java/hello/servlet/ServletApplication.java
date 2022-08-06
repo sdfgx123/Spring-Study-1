6c51751e8a85d0179f8af514ecd903863f9b9bd5
sdfgx123 <sdfgx123@naver.com>
2022-08-06 16:38:04 +0900

    20220806 servlet

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,10 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+// servletComponentScan : servlet들 찾아서 자동으로 등록해줌, 즉 서블릿 자동 등록
+@ServletComponentScan
 @SpringBootApplication
 public class ServletApplication {
 
