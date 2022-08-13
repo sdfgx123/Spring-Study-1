6d63b76c18872bed6b6026a76a2f53585589bcb5
sdfgx123 <sdfgx123@naver.com>
2022-08-13 14:19:03 +0900

    20220813

--- servlet/src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
+++ servlet/src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -4,6 +4,10 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class MemberRepositoryTest {
 
     MemberRepository memberRepository = MemberRepository.getInstance();
@@ -16,14 +20,32 @@ public class MemberRepositoryTest {
 
     @Test
     void save() {
-        // given
+        // given 이렇게 주어졌을 때
         Member member = new Member("hello", 20);
 
-        // when
+        // when 이렇게 실행했을 때
         Member savedMember = memberRepository.save(member);
 
-        // then
+        // then 결과는 이렇게 돼야 한다
         Member findMember = memberRepository.findById(savedMember.getId());
-        Assertions.assertThat(findMember).isEqualTo(savedMember);
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+
+    @Test
+    void findAll() {
+        // given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // when
+        List<Member> result = memberRepository.findAll();
+
+        // then
+        assertThat(result.size()).isEqualTo(2);
+        // 이 둘을 캐시에 가지고 있는가
+        assertThat(result).contains(member1, member2);
     }
 }
